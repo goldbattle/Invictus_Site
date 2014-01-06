1af7441198fe9d69cf5e43cca051825e48a655dd
GoldBattle <patfgeneva@gmail.com>
2014-01-05 21:38:59 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InvictusSiteV2::Application.config.secret_key_base = 'b47884307b6db98ee6eb08287e2db162f843f49cadc9c39cd16b2e37bcd1173e211d62738d88b45caa3716ffdb222a64fe5b3607e7da34eb209456954706727f'
